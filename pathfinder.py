a0c1dfab4cfe4bd5097da9c2171469273b794070
ohnihili <bpfsignorelli@gmail.com>
2024-03-28 14:35:51 +1030

    updates path

--- pathfinder.py
+++ pathfinder.py
@@ -1,21 +1,34 @@
 import sys
 
-map_size = sys.argv[1]
+map_name = sys.argv[1]
 
-print(map_size)
-
-
-# def read_map(filename):
-
-#     with open(filename, 'r') as file:
-#         # Read map size
-#         rows, cols = map(int, file.readline().split())
+def read_map(file):
+    with open(file, 'r') as file:
+        # Read map size
+        rows, cols = map(int, file.readline().split())
         
-#         # Read start and end positions
-#         start = tuple(map(int, file.readline().split()))
-#         end = tuple(map(int, file.readline().split()))
+        # Read start and end positions
+        start = tuple(map(int, file.readline().split()))
+        end = tuple(map(int, file.readline().split()))
         
-#         # Read the map itself
-#         grid = [list(map(int, line.split())) for line in file]
+        # Read the map itself
+        grid = []
+        for line in file:
+            row = []
+            for item in line.split():
+                if item == 'X':
+                    row.append('X')  # Keep 'X' as is
+                else:
+                    row.append(int(item))
+            grid.append(row)
         
-#     return rows, cols, start, end, grid
+    return rows, cols, start, end, grid
+
+rows, cols, start, end, grid = read_map(map_name)
+
+print("Map dimensions:", rows, "rows x", cols, "columns")
+print("Start position:", start)
+print("End position:", end)
+print("Grid:")
+for row in grid:
+    print(row)
